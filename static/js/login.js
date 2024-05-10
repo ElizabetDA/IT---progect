65765bf3a5792752a77bfe583ee37defbabeb6ff
Liza2398 <dyachenko.liza2017@gmail.com>
2024-05-10 20:13:51 +0300

    Первоначальная версия авторизации

---
+++ static/js/login.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Обработчик отправки формы
+    document.querySelector('form').addEventListener('submit', function(event) {
+        event.preventDefault(); // Предотвращаем отправку формы по умолчанию
+        
+        // Получаем данные из полей формы
+        var formData = new FormData(this);
+
+        // Отправляем POST-запрос на сервер
+        fetch('/login', {
+            method: 'POST',
+            body: formData,
+            headers: {
+                'X-CSRFToken': document.querySelector('input[name="csrf_token"]').value // Получаем CSRF токен из скрытого поля
+            }
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Ошибка при отправке формы');
+            }
+            return response.json();
+        })
+        .then(data => {
+            // Обрабатываем успешный ответ от сервера
+            console.log(data);
+            // Дополнительные действия, например, перенаправление пользователя на другую страницу
+        })
+        .catch(error => {
+            // Обрабатываем ошибки
+            console.error('Произошла ошибка:', error);
+        });
+    });
+});
